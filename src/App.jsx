fea3aefaa7d3d57a87938e47a5bed682cfc75041
Benny <bennyliaw@gmail.com>
2025-04-06 07:55:52 +0800

    jj quiz initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Quiz from './lib/Quiz';
+import quiz from './quiz';
+
+const App = () => {
+  const [quizResult, setQuizResult] = useState();
+
+  return (
+    <div style={{ margin: 'auto', width: '100%' }}>
+      <Quiz
+        quiz={quiz}
+        shuffle
+        shuffleAnswer
+        showInstantFeedback
+        // continueTillCorrect
+        onComplete={setQuizResult}
+        onQuestionSubmit={(obj) => console.log('user question results:', obj)}
+        // disableSynopsis
+        timer={120}
+        allowPauseTimer
+        enableProgressBar
+      />
+    </div>
+  );
+}
+
+export default App
---
+++ src/quiz.jsx
@@ -0,0 +1,154 @@
+const segment = {
+  basic: 'Basic',
+  medium: 'Medium',
+  advanced: 'Advanced',
+};
+
+const quiz =  {
+  "quizTitle": "JJ Pokemon Quiz 0.1",
+  "quizSynopsis": "Pokemon Quiz for Justin, Jacelyn, and Family",
+  "questions": [
+    {
+      "question": "What was the first Pokémon ever created?",
+      "questionType": "text",
+      "answers": [
+        "Pikachu",
+        "Bulbasaur",
+        "Rhydon",
+        "Mew"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "That's right. It is Rhydon.",
+      "messageForIncorrectAnswer": "No, not that. Wrong answer.",
+      "point": "10"
+    },
+    {
+      "question": "Which Pokémon is known as the “Eon Pokémon”?",
+      "questionType": "text",
+      "answers": [
+        "Lugia",
+        "Mewtwo",
+        "Latios and Latias",
+        "Rayquaza"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "You got it!",
+      "messageForIncorrectAnswer": "Wrong answer!",
+      "point": "10"
+    },
+    {
+      "question": "What type is the Pokémon “Garchomp”?",
+      "answers": [
+        "Dragon/Ground",
+        "Dragon/Flying",
+        "Ground/Rock",
+        "Dragon/Steel"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "That's right!",
+      "messageForIncorrectAnswer": "Wrong.",
+      "point": "10",
+      "questionType": "text"
+    },
+    {
+      "question": "In which generation was the Fairy-type introduced?",
+      "answers": [
+        "Generation IV",
+        "Generation V",
+        "Generation VI",
+        "Generation VII"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "Correct!",
+      "messageForIncorrectAnswer": "Sorry, wrong answer!",
+      "questionType": "text",
+      "point": "10"
+    },
+    {
+      "question": "Which of the following Pokémon is not a Legendary Pokémon?",
+      "answers": [
+        "Entei",
+        "Garchomp",
+        "Regirock",
+        "Cresselia"
+      ],
+      "questionType": "text",
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "Yes, correct. Garchomp is not a legendary Pokemon!",
+      "messageForIncorrectAnswer": "Not quite right",
+      "point": "10"
+    },
+    {
+      "question": "What is the name of the region in Pokémon Sun and Moon?",
+      "questionType": "text",
+      "answers": [
+        "Sinnoh",
+        "Hoenn",
+        "Alola",
+        "Unova"
+      ],
+      "correctAnswer": "3",
+      "messageForCorrectAnswer": "That is correct!",
+      "messageForIncorrectAnswer": "Wrong!",
+      "point": "10"
+    },
+    {
+      "question": "Which Pokémon can evolve into multiple different forms depending on certain conditions?",
+      "questionType": "text",
+      "answers": [
+        "Eevee",
+        "Ditto",
+        "Mew",
+        "Pikachu"
+      ],
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "Right!",
+      "messageForIncorrectAnswer": "Wrong!",
+      "point": "10"
+    },
+    {
+      "question": "What move does the most damage to a Ghost-type Pokémon?",
+      "questionType": "text",
+      "answers": [
+        "Psychic",
+        "Shadow Ball",
+        "Hyper Beam",
+        "Crunch"
+      ],
+      "correctAnswer": "4",
+      "messageForCorrectAnswer": "Yes!",
+      "messageForIncorrectAnswer": "Nope",
+      "point": "10"
+    },
+    {
+      "question": "Which Pokémon is known for having the signature move “Spacial Rend”?",
+      "questionType": "text",
+      "answers": [
+        "Palkia",
+        "Dialga",
+        "Giratina",
+        "Arceus"
+      ],
+      "point": "10",
+      "correctAnswer": "1",
+      "messageForCorrectAnswer": "You are right!",
+      "messageForIncorrectAnswer": "Wrong answer"
+    },
+    {
+      "question": "Who is the current champion of the Galar region in Pokémon Sword and Shield?",
+      "questionType": "text",
+      "answers": [
+        "Hop",
+        "Leon",
+        "Bea",
+        "Raihan"
+      ],
+      "point": "10",
+      "correctAnswer": "2",
+      "messageForCorrectAnswer": "You got it!",
+      "messageForIncorrectAnswer": "No, not that!"
+    }
+  ]
+};
+
+export default quiz;
